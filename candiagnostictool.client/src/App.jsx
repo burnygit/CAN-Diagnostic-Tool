9352748ece66b0c0bcd6edcbee6fdc7e0c137c0e
burnygit <adamburny0@gmail.com>
2024-12-21 17:59:07 +0100

    Added useEffect to prevent WebSocket duplication

--- candiagnostictool.client/src/App.jsx
+++ candiagnostictool.client/src/App.jsx
@@ -3,23 +3,31 @@ import './App.css';
 
 function App() {
 
-    const ws = new WebSocket("wss://localhost:7199/ws/can");
+    //useEffect aby zapobiec duplikowaniu sie WS
+    useEffect(() => {
+        const ws = new WebSocket("wss://localhost:7199/ws/can");
 
-    ws.onopen = () => {
-        console.log("WebSocket connection established (Połączono WebSocket)");
-    };
+        ws.onopen = () => {
+            console.log("WebSocket connection established (Połączono WebSocket)");
+        };
 
-    ws.onmessage = (event) => {
-        console.log("Received CAN message: ", JSON.parse(event.data));
-    };
+        ws.onmessage = (event) => {
+            console.log("Received CAN message: ", JSON.parse(event.data));
+        };
 
-    ws.onclose = () => {
-        console.log("WebSocket connection closed (Zamknięto połączenie z WebSocket)");
-    };
+        ws.onclose = () => {
+            console.log("WebSocket connection closed (Zamknięto połączenie z WebSocket)");
+        };
 
-    ws.onerror = (error) => {
-        console.error("Websocket error:", error);
-    } 
+        ws.onerror = (error) => {
+            console.error("Websocket error:", error);
+        };
+
+        return () => {
+            ws.close();
+        }
+}, [])
+    
 
 
     return (
