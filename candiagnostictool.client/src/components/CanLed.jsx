75af154583874fee1f9f2e7a027aab933562dc82
burnygit <adamburny0@gmail.com>
2024-12-28 16:50:01 +0100

    Created and stylized CanLed component

---
+++ candiagnostictool.client/src/components/CanLed.jsx
@@ -0,0 +1,40 @@
+import './CanLed.css';
+
+import { useState, useEffect } from 'react';
+
+import { useWebSocketContext } from './WebSocketContext';
+
+const CanLed = ({ledLabel, identifier, byte, bitMask }) => {
+
+    const [color, setColor] = useState("led-off");
+
+    const data = useWebSocketContext();
+
+    useEffect(() => {
+        const frame = data[identifier];
+        if (frame && frame.Data) {
+            const ledState = analyzeLed(frame, byte, bitMask);
+            setColor(ledState ? 'led-on' : 'led-off');
+        }
+
+    }, [data, identifier, byte, bitMask]);
+
+
+    const analyzeLed = (frame, byte, bit_mask) => {
+
+        const value = frame.Data[byte];
+        const ledState = (value & bit_mask) !== 0;
+        return ledState;
+    };
+
+    return (
+        <div className={`led-container`}>
+
+            <div className={`led  ${color} `}> </div>
+            <div className="label"> {ledLabel} </div>
+
+        </div>
+    )
+}
+
+export default CanLed;
--- candiagnostictool.client/src/components/CanReceived.jsx
+++ candiagnostictool.client/src/components/CanReceived.jsx
@@ -1,6 +1,11 @@
+﻿//[!] Dodac ewentualnie walidację propTypes do komponentów
+
+import './CanReceived.css'
+
 import { WebSocketProvider } from './WebSocketContext';
 
 import CanAnalogValue from './CanAnalogValue';
+import CanLed from './CanLed';
 
 const CanReceived = () => {
 
@@ -10,16 +15,69 @@ const CanReceived = () => {
         <>
             <div className="receivedValues">
                 <WebSocketProvider url="wss://localhost:7199/ws/can">
-                <CanAnalogValue
-                    Label="Analog Test 4"
-                    Unit="A"
-                    Identifier={0x3C5}
-                    startByte={2}
-                    length={2}
-                    factor={10}
-                    decimalPlaces={1}
-                    maxValue={150}
+
+             
+                    <CanAnalogValue
+                        Label="Analog Test 4"
+                        Unit="A"
+                        Identifier={0x3C5}
+                        startByte={2}
+                        length={2}
+                        factor={10}
+                        decimalPlaces={1}
+                        maxValue={150}
                     />
+
+                    <CanAnalogValue
+                        Label="Analog Test 5"
+                        Unit="A"
+                        Identifier={0x3C6}
+                        startByte={0}
+                        length={2}
+                        factor={100}
+                        decimalPlaces={2}
+                        maxValue={150}
+                    />
+
+                    <CanAnalogValue
+                        Label="Analog Test 6"
+                        Unit="A"
+                        Identifier={0x3C7}
+                        startByte={0}
+                        length={2}
+                        factor={1000}
+                        decimalPlaces={1}
+                        maxValue={150}
+                    />
+
+                    <CanLed
+                        ledLabel="LED 1"
+                        identifier={0x123}
+                        byte={0}
+                        bitMask={0x01}
+                    />
+
+                    <CanLed
+                        ledLabel="LED 1"
+                        identifier={0x123}
+                        byte={0}
+                        bitMask={0x02}
+                    />
+
+                    <CanLed
+                        ledLabel="LED 1"
+                        identifier={0x123}
+                        byte={0}
+                        bitMask={0x04}
+                    />
+
+                    <CanLed
+                        ledLabel="LED 1"
+                        identifier={0x123}
+                        byte={0}
+                        bitMask={0x08}
+                    />
+
                 </WebSocketProvider>
             </div>
         </>
